087f6cc2da54b0cff75f9cce91156ab0f5ccfd79
Chasingbob <dries.cronje@outlook.com>
2017-02-07 05:36:27 +0200

    Added SqueeseNet implementation to train on the classic Kaggle Cats vs Dogs image set

--- cats_vs_dogs.py
+++ cats_vs_dogs.py
@@ -1,19 +1,69 @@
 import model
+from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
 
 def main():
+    np.random.seed(44)
     nb_class = 2
     width, height = 224, 224
 
 
-    model = SqueezeNet(
-        nb_class, inputs=(3, args.height, args.width))
-    dp.visualize_model(model)
+    sn = model.SqueezeNet(nb_classes=nb_class, inputs=(3, height, width))
 
     print('Build model')
 
-    sgd = SGD(lr=0.1, decay=0.0002, momentum=0.9, nesterov=True)
-    model.compile(
+    sgd = SGD(lr=0.001, decay=0.0002, momentum=0.9, nesterov=True)
+    sn.compile(
         optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Training
+    train_data_dir = 'data/train'
+    validation_data_dir = 'data/validation'
+    nb_train_samples = 2000
+    nb_validation_samples = 800
+    nb_epoch = 1000
+
+    #   Generator
+    #train_datagen = ImageDataGenerator(
+    #        rescale=1./255,
+    #        shear_range=0.2,
+    #        zoom_range=0.2,
+    #        horizontal_flip=True)
+    train_datagen = ImageDataGenerator(rescale=1./255)
+
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+            train_data_dir,
+            target_size=(width, height),
+            batch_size=32,
+            class_mode='categorical')
+
+    validation_generator = test_datagen.flow_from_directory(
+            validation_data_dir,
+            target_size=(width, height),
+            batch_size=32,
+            class_mode='categorical')
+
+    sn.fit_generator(
+            train_generator,
+            samples_per_epoch=nb_train_samples,
+            nb_epoch=nb_epoch,
+            validation_data=validation_generator,
+            nb_val_samples=nb_validation_samples)
+
+    sn.save_weights('weights.h5')
+
+
+
+    # Evaluate
+
+
+    # Predict
+
+
+    # Save Weights
 
 #    print("Training...")
 #    model.fit_generator(
